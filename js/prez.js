3af26f3e02a61604f0caff0b0ea7f70594351a85
Guillaume Wuip <guillaume@wuips.com>
2016-08-10 08:09:54 +0200

    Empty index.html

---
+++ js/prez.js
@@ -0,0 +1,51 @@
+
+'use strict';
+
+(function () {
+
+    Reveal.initialize({
+        controls: true,
+        progress: true,
+        history: true,
+        center: true,
+
+        transition: 'none',
+
+        dependencies: [
+            {
+                src: 'lib/js/classList.js',
+                condition: function() {
+                    return !document.body.classList;
+                }
+            },
+            {
+                src: 'plugin/markdown/marked.js',
+                condition: function() {
+                    return !!document.querySelector('[data-markdown]');
+                }
+            },
+            {
+                src: 'plugin/markdown/markdown.js',
+                condition: function() {
+                    return !!document.querySelector('[data-markdown]');
+                }
+            },
+            {
+                src: 'plugin/highlight/highlight.js',
+                async: true,
+                callback: function() {
+                    hljs.initHighlightingOnLoad();
+                }
+            },
+            {
+                src: 'plugin/zoom-js/zoom.js',
+                async: true
+            },
+            {
+                src: 'plugin/notes/notes.js',
+                async: true
+            },
+        ]
+    });
+
+}());
