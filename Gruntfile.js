5a0b165fc68b5e6f8b21d2d4a22af5a6f52c25fa
Guillaume Wuip <guillaume@wuips.com>
2016-08-10 19:13:42 +0200

    Remove reveal tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,10 +17,6 @@ module.exports = function(grunt) {
                 ' */'
         },
 
-        qunit: {
-            files: [ 'test/*.html' ]
-        },
-
         uglify: {
             options: {
                 banner: '<%= meta.banner %>\n'
@@ -139,7 +135,6 @@ module.exports = function(grunt) {
     });
 
     // Dependencies
-    grunt.loadNpmTasks( 'grunt-contrib-qunit' );
     grunt.loadNpmTasks( 'grunt-contrib-jshint' );
     grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
     grunt.loadNpmTasks( 'grunt-contrib-uglify' );
@@ -153,7 +148,7 @@ module.exports = function(grunt) {
     grunt.registerTask( 'default', [ 'css', 'js' ] );
 
     // JS task
-    grunt.registerTask( 'js', [ 'jshint', 'uglify', 'qunit' ] );
+    grunt.registerTask( 'js', [ 'jshint', 'uglify' ] );
 
     // Theme CSS
     grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
@@ -169,8 +164,5 @@ module.exports = function(grunt) {
 
     // Serve presentation locally
     grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
-
-    // Run tests
-    grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
 
 };
