2aad2abdb94383277e390f3b3809221fa5098d1b
Guillaume Wuip <clochard.guillaume@gmail.com>
2016-08-17 16:00:48 +0200

    Init eslintrc

---
+++ demo/gulpfile.js
@@ -0,0 +1,45 @@
+
+const
+    gulp   = require('gulp'),
+    eslint = require('gulp-eslint');
+
+const inputPaths = {
+    javascript: [
+        'gulpfile.js',
+        'index.js',
+        'src/*.js',
+        'test/*.js',
+    ],
+};
+
+/**
+ * JS ES6
+ */
+
+gulp.task('js:lint', () => {
+    // http://eslint.org/docs/rules
+    let task = gulp
+        .src(inputPaths.javascript)
+        .pipe(eslint())
+        .pipe(eslint.format());
+
+    if (process.env.CI) {
+        task = task.pipe(eslint.failAfterError());
+    }
+
+    return task;
+});
+
+gulp.task('js:watch', () => {
+    gulp.watch(inputPaths.javascript, ['js:lint']);
+});
+
+/**
+ * Main tasks
+ */
+
+gulp.task('watch', ['js:watch']);
+gulp.task('lint', ['js:lint']);
+gulp.task('build', ['lint']);
+gulp.task('default', ['build', 'watch']);
+
